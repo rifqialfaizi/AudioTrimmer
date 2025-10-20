50c3066e67d35ecb939750b80d502944c0804f26
Rifqi Alfaizi <rifqi.alfaizi@phincon.com>
2025-10-20 10:51:37 +0700

    Initial Commit

---
+++ AudioTrimmer/AudioTrimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTrimmerApp.swift
+//  AudioTrimmer
+//
+//  Created by Rifqi Alfaizi on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioTrimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
